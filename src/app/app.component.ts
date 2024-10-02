d8c5771e6c5d8dc5f727d9f551b64b4131b25f29
amauris <amauris>
2024-10-02 10:41:09 -0400

    2nd step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { WorkComponent } from '../work/work.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { WorkComponent } from "../work/work.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, WorkComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'Portfolio Angular';
+  title = "Portfolio Angular";
 }
